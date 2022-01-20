dc87c6c677cdd9dd981db0857861ec9967f1e52c
Gobousei <93245017+Gobousei@users.noreply.github.com>
2022-01-20 16:13:50 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 'use strict';
  
+const str = '吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニヤーニヤー泣いていた事だけは記憶して居る。';
+ 
 //ゲームのオブジェクトを640×480サイズで作る
 let game = new Game( 640, 480 );
  
 //ラベルオブジェクトを作る
-let label = new Label( 'こんにちは' );
+let label = new Label( str );
+label.interval = 10;
  
 //add()を使って、ゲームにラベルを表示
-game.add( label, 20, 40 );
+game.add( label, 0, 0 );
  
 //ゲームスタート
 game.start();
