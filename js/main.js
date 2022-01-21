e64f847cd33e602a0aaee67f6a27bcea4f54d70c
Gobousei <93245017+Gobousei@users.noreply.github.com>
2022-01-21 17:53:50 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-docment.whrite("あいうえお")
+'use strict'
+
+//ブラウザがページを完全に読みこむまで待つ
+addEventListener( 'load', () => {
+
+	//変数gameの指定
+	const game = new Game();
+
+	//gameにgame startを命令
+	game.start();
+
+} );
